75215aba5c2c9059f648b780a968e780fa112672
Chunghyun Lim <104333014+potterLim@users.noreply.github.com>
2026-02-08 19:43:37 +0900

    refactor: introduce value objects for course id/section/period

---
+++ Domain/CourseId.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TimetableGenerator
+{
+    public readonly struct CourseId : IEquatable<CourseId>
+    {
+        public int Value { get; }
+
+        public CourseId(int value)
+        {
+            Value = value;
+        }
+
+        public bool Equals(CourseId other)
+        {
+            return Value == other.Value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is CourseId other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value;
+        }
+
+        public static bool operator ==(CourseId left, CourseId right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(CourseId left, CourseId right)
+        {
+            return !left.Equals(right);
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
---
+++ Domain/Period.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TimetableGenerator
+{
+    public readonly struct Period : IEquatable<Period>
+    {
+        public int Value { get; }
+
+        public Period(int value)
+        {
+            Value = value;
+        }
+
+        public bool IsValid()
+        {
+            return Value > 0;
+        }
+
+        public bool Equals(Period other)
+        {
+            return Value == other.Value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Period other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public static bool operator ==(Period left, Period right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Period left, Period right)
+        {
+            return !left.Equals(right);
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
